689f5e6498ae8df880fd052743da07f7a528e858
Robert Altman <rjaltman04@gmail.com>
2017-02-25 16:15:20 -0600

    Added Routing for Lines and Stops, Edited Controllers

--- app/controllers/home_controller.rb
+++
@@ -1,5 +0,0 @@
-class HomeController < ApplicationController
-  def index
-      render 'home'
-  end
-end
---
+++ app/controllers/lines_controller.rb
@@ -0,0 +1,6 @@
+class LinesController < ApplicationController
+  def index
+  end
+  def show
+  end
+end
