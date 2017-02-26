1e88e3c7ae018971a864d6d474dfbeb46b2732a0
Marc-Andre Lafortune <github@marc-andre.ca>
2017-02-26 10:16:11 -0600

    create new_issue_button helper

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -1,2 +1,9 @@
 module IssuesHelper
+  def new_issue_button(line: nil, stop: nil)
+    line ||= stop&.lines&.first
+    link_to(
+      content_tag(:div, '', class: 'create-issue'),
+      new_issue_path(line_id: line&.id, stop_id: stop&.id),
+    )
+  end
 end
