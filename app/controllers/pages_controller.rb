f726f3f10d6059bf540e49ab72665d684e3d3894
Viktor Koves <viktorkoves@gmail.com>
2017-02-28 01:09:34 -0600

    Increased location range to 0.5 miles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def dashboard
       if ((params[:lat]) && (params[:long]))
-          @nearby_stops = Stop.includes(:lines).within(0.2, :origin => [params[:lat],params[:long]])
+          @nearby_stops = Stop.includes(:lines).within(0.5, :origin => [params[:lat],params[:long]])
       end
   end
 
