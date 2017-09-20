a355ddc5a2da2c6df5b1d4e0bd20a8de05908b2d
Viktor Koves <viktorkoves@gmail.com>
2017-09-19 20:51:16 -0500

    Made settings page only reachable if you are signed in as an admin

--- config/initializers/rails_settings_ui.rb
+++ config/initializers/rails_settings_ui.rb
@@ -3,7 +3,7 @@ require 'rails-settings-ui'
 #= Application-specific
 #
 # # You can specify a controller for RailsSettingsUi::ApplicationController to inherit from:
-# RailsSettingsUi.parent_controller = 'Admin::ApplicationController' # default: '::ApplicationController'
+# RailsSettingsUi.parent_controller = 'AdminController::ApplicationController' # default: '::ApplicationController'
 #
 # # Render RailsSettingsUi inside a custom layout (set to 'application' to use app layout, default is RailsSettingsUi's own layout)
 # RailsSettingsUi::ApplicationController.layout 'admin'
@@ -11,6 +11,15 @@ require 'rails-settings-ui'
 Rails.application.config.to_prepare do
   # If you use a *custom layout*, make route helpers available to RailsSettingsUi:
   RailsSettingsUi.inline_main_app_routes!
+
+  RailsSettingsUi::ApplicationController.module_eval do
+    before_action :check_settings_permissions # starting from Rails 4
+
+    private
+    def check_settings_permissions
+      redirect_to("/") unless current_user and current_user.admin
+    end
+  end
 end
 
 RailsSettingsUi.setup do |config|
