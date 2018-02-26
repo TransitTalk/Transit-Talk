9599c5e39f2afacd070b74c6ce9ac9c0da5be912
Blake Pfaff <blake.a.pfaff2@gmail.com>
2018-02-26 13:08:46 -0600

    adds tests for fav button

---
+++ test/controllers/favorites_controller_test.rb
@@ -0,0 +1,74 @@
+require 'test_helper'
+
+class FavoritesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+  #setup do
+    #@favorite = favorites(:one)
+  #end
+
+  test "logged in user should get the favorites page" do
+    # Sign in user
+    sign_in users(:one)
+
+    get favorites_url
+    assert_response :success
+  end
+
+  test "unauth user is redirected to log in " do
+    get favorites_url
+    assert_redirected_to new_user_session_path
+  end
+
+  test "creating a new favorite will increase the count by one" do
+    # Create user
+    user = users(:one)
+
+    # Create a Stop model
+    @stop = Stop.create(
+      :api_id       => "test_id",
+      :name         => "Test Stop",
+      :longitude    => 100,
+      :lattitude    => 100,
+      :twin_stop_id => 1,
+      :onestop_id   => "test"
+    )
+
+    # Sign in user
+    sign_in user
+
+    # Assert that when the GET request happens
+    # the current user's favorites count is not the same
+    # as when it started the request
+    assert_difference 'user.favorites.count' do
+      get "/favorite/#{@stop.id}"
+    end
+  end
+
+  test "creating a new favorite will decrease the count by one" do
+    # Create user
+    user = users(:one)
+
+    # Create a Stop model
+    @stop = Stop.create(
+      :api_id       => "test_id",
+      :name         => "Test Stop",
+      :longitude    => 100,
+      :lattitude    => 100,
+      :twin_stop_id => 1,
+      :onestop_id   => "test"
+    )
+
+    # Sign in user
+    sign_in user
+
+    get "/favorite/#{@stop.id}"
+
+    # Assert that when the GET request happens
+    # the current user's favorites count is not the same
+    # as when it started the request
+    assert_difference 'user.favorites.count', -1 do
+      get "/unfavorite/#{@stop.id}"
+    end
+  end
+
+end
