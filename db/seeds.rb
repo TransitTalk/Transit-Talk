32db10abcc1680ec8548dc087575c014957da996
Nolan Neeley <nolanneeley@gmail.com>
2018-03-19 13:07:33 -0500

    Add train line id, name, and colors to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+lines_list = [
+  [ 1, "Orange Line", "ff2800" ],
+  [ 2, "Brown Line", "67340f" ],
+  [ 3, "Green Line", "00a32f" ],
+  [ 4, "Pink Line", "f072a5" ],
+  [ 5, "Blue Line", "009fe5" ],
+  [ 6, "Yellow Line", "fae900" ],
+  [ 7, "Purple Line", "5c009d" ],
+  [ 8, "Red Line", "d60019" ]
+]
+
+lines_list.each do |id, name, color|
+  Line.create( id: id, name: name, color:color )
+end
