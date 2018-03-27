a2ba4aa70f61a7b170bdd860601b7b750a8b7bf1
Edwin Flores <eflores003@gmail.com>
2018-03-27 15:27:40 +0000

    fixed identation to tabs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -64,36 +64,36 @@ $(document).ready(function()
 	
 	//this code is for sorting DOM elements by id or name
 	jQuery.fn.sortDomElements = function(comparator) {
-	  return Array.prototype.sort.call(this, comparator).each(function(i) {
-	    this.parentNode.appendChild(this);
-	  });
+		return Array.prototype.sort.call(this, comparator).each(function(i) {
+	    	this.parentNode.appendChild(this);
+		});
 	};
 	$("#toSortName").click(function (){
 		$(".page-container .stop-partial").sortDomElements(function(a,b){
-	    akey = $(a).children('span').text();
-	    bkey = $(b).children('span').text();
-	    if (akey == bkey) return 0;
-	    if (akey < bkey) return -1;
-	    if (akey > bkey) return 1;
+			akey = $(a).children('span').text();
+			bkey = $(b).children('span').text();
+			if (akey == bkey) return 0;
+			if (akey < bkey) return -1;
+			if (akey > bkey) return 1;
 		});
 	});
 	$("#toSortId").click(function (){
 		$(".page-container .stop-partial").sortDomElements(function(a,b){
-	    akey = $(a).children('.line-tile').text();
-	    bkey = $(b).children('.line-tile').text();
-	    //check if values are integer or string or empty string
-	    akeyRegex = /[a-zA-Z]./.test(akey); //checks values in the form of X94, but not 52A
-	    bkeyRegex = /[a-zA-Z]./.test(bkey);
-	    aint = parseInt(akey)? parseInt(akey) : (akeyRegex)? 0.2:0;
-	    bint = parseInt(bkey)? parseInt(bkey) : (bkeyRegex)? 0.2:0;
-	    //Case to sort when both values are non-empty strings
-	    if(akeyRegex && bkeyRegex){
-	    	aint = akey;
-	    	bint = bkey;
-	    }
-	    if (aint == bint) return 0;
-	    if (aint < bint) return -1;
-	    if (aint > bint) return 1;
+			akey = $(a).children('.line-tile').text();
+			bkey = $(b).children('.line-tile').text();
+			//check if values are integer or string or empty string
+			akeyRegex = /[a-zA-Z]./.test(akey); //checks values in the form of X94, but not 52A
+			bkeyRegex = /[a-zA-Z]./.test(bkey);
+			aint = parseInt(akey)? parseInt(akey) : (akeyRegex)? 0.2:0;
+			bint = parseInt(bkey)? parseInt(bkey) : (bkeyRegex)? 0.2:0;
+			//Case to sort when both values are non-empty strings
+			if(akeyRegex && bkeyRegex){
+				aint = akey;
+				bint = bkey;
+			}
+			if (aint == bint) return 0;
+			if (aint < bint) return -1;
+			if (aint > bint) return 1;
 		});
 	});
 });
