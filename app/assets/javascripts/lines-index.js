3851fe3bfb446de3b4a1210bf144fa6070c4a269
Edwin Flores <eflores003@gmail.com>
2018-04-19 16:41:37 +0000

    added block comments describe js functions

--- app/assets/javascripts/lines-index.js
+++ app/assets/javascripts/lines-index.js
@@ -7,12 +7,22 @@ $(document).ready(function()
 		});
 	};
 
+	// Function to compare two numbers or strings: akey and bkey
 	function comparePair(akey,bkey) {
 		if (akey == bkey) return 0;
 		if (akey < bkey) return -1;
 		if (akey > bkey) return 1;
 	}
 
+	/* Function to sort DOM elements in a hierachical tree structure where: 
+	parentSelector is a jquery selector that specifies the top DOM element in a tree
+	whose children elements are desired to be sorted.
+	childSelector is a jquery selector that selects all children DOM elements of the tree
+	that we want to be sorted
+	id is an number parameter that specifies whether to use a simple comparison function
+	for sorting  (id=0 is default) or use a more customized script for complex comparisons 
+	(id=1). In this case, the complex comparisons involve strings, empty strings, and numbers 
+	for line titles.*/
 	function sortLineDom(parentSelector, childSelectorCompare, id=0) {
 		$(parentSelector).sortDomElements(function(a,b){
 			akey = $(a).children(childSelectorCompare).text();
@@ -34,7 +44,13 @@ $(document).ready(function()
 			}
 		});
 	};
-
+	
+	/* Function for a clicking event that will sort DOM elements in a hierachical tree structure,
+	remove active class from specified html buttons, and attach an active class on the html button 
+	clicked on.
+	buttonId is a jquery selector for the id that belongs to the html button on the index page that 
+	will call the sortLineDOM function.
+	parentSelector, childSelector, and id are all parameters used for the sortLineDom function 	*/
 	function clickSort(buttonId, parentSelector, childSelectorCompare, id=0) {
 		$(buttonId).click( function () {
 			$("#sort-name, #sort-id").removeClass('active');
