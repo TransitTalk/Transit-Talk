60f91ae698c141002404cc7a79297df07c6ec3dd
arieldiamond <diamond@jellyvision.com>
2018-04-24 20:23:41 -0500

    update issue tests and add validation to issue model

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -3,4 +3,9 @@ class Issue < ApplicationRecord
   belongs_to :stop, foreign_key: "stop_onestop_id"
   belongs_to :vehicle, required: false
   belongs_to :line, foreign_key: "line_onestop_id"
+
+  validates :issue_type, presence: true
+  validates :location_type, presence: true
+  validates :reported_at, presence: true
+  validates :stop_onestop_id, presence: :true
 end
--- test/models/issue_test.rb
+++ test/models/issue_test.rb
@@ -4,17 +4,16 @@ class IssueTest < ActiveSupport::TestCase
 
   # Verify issue is valid if correct fields are provided
   test 'valid issue' do
-    issue = Issue.new(types: 'Cleanliness')
+    issue = Issue.new(issue_type: 'Cleanliness', location_type: 'bus', reported_at: Time.now, stop_onestop_id: '1')
     assert issue.valid?
   end
 
   # Verify correct errors are raised if an issue is created
-  # without the proper fields. 
-  test 'invalid without types' do
-    line = lines(:one)
-    issue = Issue.new(line: line)
+  # without the proper fields.
+  test 'invalid without location_type, reported_at, or stop_onestop_id' do
+    issue = Issue.new(issue_type: 'Cleanliness')
     refute issue.valid?
-    assert_not_nil issue.errors[:types]
+    assert_not_nil issue.errors[:issue_type]
   end
 
 end
