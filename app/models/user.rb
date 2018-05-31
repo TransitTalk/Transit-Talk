d69dd6ec7be069b41ba4f59543a3ad5c1ac9f277
codeauslander <cesar.gusto.catano@gmail.com>
2018-05-30 23:53:07 -0500

    remove spaces

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
   has_many :issues
   has_and_belongs_to_many :favorites, class_name: "Stop", association_foreign_key: "stop_onestop_id"
 end
