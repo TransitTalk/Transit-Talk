8249095a364ef61c2a6a2aa6d316fc0c4a299291
codeauslander <cesar.gusto.catano@gmail.com>
2018-06-05 20:55:38 -0500

    added singrid gem, fixed password reset link

--- Gemfile
+++ Gemfile
@@ -50,6 +50,8 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'sendgrid-ruby'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,15 @@
 # Load the Rails application.
 require_relative 'application'
 
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'yourdomain.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
 # Initialize the Rails application.
 Rails.application.initialize!
