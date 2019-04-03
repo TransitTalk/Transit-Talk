a62c4bc2110f1c6b07a02cd094cae4722a6fc7ed
Evan Grillo <evan.grillo@outlook.com>
2019-04-02 20:08:04 -0500

    production mailer configuration for devise password reset

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,17 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    user_name:      ENV['GMAIL_USERNAME'],
+    password:       ENV['GMAIL_PASSWORD'],
+    domain:         'gmail.com',
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
+
 end
