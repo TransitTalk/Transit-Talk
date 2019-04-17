5b72c8fd32cb23d410cca0e7352d8316c701a386
Sinclair Target <sinclairtarget@gmail.com>
2019-04-17 08:02:55 -0500

    Placate rubocop.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,23 +4,23 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   setup do
-    User.create(email: 'badperson@gmail.com',
-                password: 'reallysecure',
-                password_confirmation: 'reallysecure')
+    User.create(email: "badperson@gmail.com",
+                password: "reallysecure",
+                password_confirmation: "reallysecure")
   end
 
-  test 'banning user bans correct user' do
-    user = User.find_by_email('badperson@gmail.com')
+  test "banning user bans correct user" do
+    user = User.find_by_email("badperson@gmail.com")
     assert_not user.is_banned
 
-    User.ban_user!('badperson@gmail.com')
+    User.ban_user!("badperson@gmail.com")
 
     user.reload
     assert user.is_banned
   end
 
-  test 'banning ignores bad email and does not throw error' do
-    User.ban_user!('notauser@gmail.com')
-    User.ban_user!('')
+  test "banning ignores bad email and does not throw error" do
+    User.ban_user!("notauser@gmail.com")
+    User.ban_user!("")
   end
 end
