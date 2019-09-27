30018b1061d53c9c2bb5bb604e6c2d0275650be3
Adam Blaida <ajblaida@gmail.com>
2019-09-26 22:40:52 -0500

    work on system test

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,4 +2,8 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_firefox, screen_size: [1400, 1400]
+
+  def page
+    Capybara.current_session
+  end
 end
