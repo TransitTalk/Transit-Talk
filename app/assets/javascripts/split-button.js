ffd5a579e6365c0235ed1cc39e4aa6fc95324533
Tomer Kimia <tkimia@jellyvision.com>
2019-11-10 10:51:18 -0600

    need to roll back to es5 for heroku

--- app/assets/javascripts/split-button.js
+++ app/assets/javascripts/split-button.js
@@ -4,17 +4,17 @@ $(document).on('ready turbolinks:load', function() {
     $('button.split-button')
         .click(function() {
             // Get button Id which was clicked
-            let targetId = $(this).attr('id');
+            var targetId = $(this).attr('id');
             // Split it into an array
-            let newShownTableIdAsArray = targetId.split('_');
+            var newShownTableIdAsArray = targetId.split('_');
 
             newShownTableIdAsArray.pop(); // Remove the "button" at the end
 
             // Join the array without button, should correspond with a div to show/hide
-            let newShownTableId = newShownTableIdAsArray.join('_');
+            var newShownTableId = newShownTableIdAsArray.join('_');
 
             // Grab that div with id
-            let newTableToShow = $('#'+newShownTableId);
+            var newTableToShow = $('#'+newShownTableId);
 
             //hide siblings
             newTableToShow.siblings('.toggleable-list').hide();
