6fd13f4b7d6883bf8c194fc7853bf7f934213d4e
Adam Blaida <ajblaida@gmail.com>
2019-11-26 10:24:06 -0600

    Finish implementation, css to go

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -3,7 +3,8 @@
 module IssuesHelper
   def new_issue_button(line: nil, stop: nil)
     line ||= stop&.lines&.first
-    link_to("",
+    link_to("Report
+    Issue",
       new_issue_path(line_id: line&.id, stop_id: stop&.id),
       id: "create-issue-#{stop&.id}", class: "create-issue"
     )
