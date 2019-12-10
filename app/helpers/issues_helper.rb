a1a5db846a133ecc700d41934c97145d901f88c6
Adam Blaida <ajblaida@gmail.com>
2019-12-09 21:31:40 -0600

    Try to add absolutely positioned links, give up, only have link cover middle

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -3,8 +3,7 @@
 module IssuesHelper
   def new_issue_button(line: nil, stop: nil)
     line ||= stop&.lines&.first
-    button_to("Report
-    Issue",
+    button_to("+",
       new_issue_path(line_id: line&.id, stop_id: stop&.id),
       id: "create-issue-#{stop&.id}", class: "create-issue nested-link-button",
       method: :get
