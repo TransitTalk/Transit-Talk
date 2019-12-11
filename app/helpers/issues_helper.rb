e5c18d49ffa43a408a2b84d572964cc4e1533a90
Tomer Kimia <tkimia@jellyvision.com>
2019-12-10 20:24:28 -0600

    revert report button to link as well

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -3,10 +3,9 @@
 module IssuesHelper
   def new_issue_button(line: nil, stop: nil)
     line ||= stop&.lines&.first
-    button_to("+",
+    link_to("Report\nIssue",
       new_issue_path(line_id: line&.id, stop_id: stop&.id),
-      id: "create-issue-#{stop&.id}", class: "create-issue nested-link-button",
-      method: :get
+      id: "create-issue-#{stop&.id}", class: "create-issue"
     )
   end
 end
