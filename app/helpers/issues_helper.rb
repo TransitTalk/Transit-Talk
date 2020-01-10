c83e699ea06ae32427b1de17ffb783d6030e30ad
Adam Blaida <ajblaida@gmail.com>
2020-01-09 21:46:00 -0600

    Add start of labels

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -5,7 +5,8 @@ module IssuesHelper
     line ||= stop&.lines&.first
     link_to("Report\nIssue",
       new_issue_path(line_id: line&.id, stop_id: stop&.id),
-      id: "create-issue-#{stop&.id}", class: "create-issue"
+      id: "create-issue-#{stop&.id}", class: "create-issue",
+      aria: { label: "Report issue at " + stop.name }
     )
   end
 end
